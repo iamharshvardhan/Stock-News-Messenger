327f5542f1d821eca2a8bab9aaaee4f7ae731858
Harshvardhan Rana <ranaharshvardhan52@gmail.com>
2023-01-28 15:02:57 +0530

    First commit

---
+++ Code/main.py
@@ -0,0 +1,65 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+VIRTUAL_TWILIO_NUMBER = "Phone_Number_From_Twilio "
+VERIFIED_NUMBER = "Target_Phone_Number"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "API_KEY"
+NEWS_API_KEY = "API_KEY"
+
+account_sid = "Twilio_SID"
+auth_token = "Twilio_Account_Token"
+
+stock_params = {
+    "function": "TIME_SERIES_WEEKLY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Weekly Time Series"]
+
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "📉"
+else:
+    up_down = "📈"
+
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    three_articles = articles[:3]
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+
+    client = Client(account_sid, auth_token)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
---
+++ README.md
@@ -0,0 +1,7 @@
+# Stock News Messenger
+
+## Description
+This program uses 3 API's to:
+1. Gain information about the stock market value of the company you want. (Alpha Vantage)
+2. Find relevent news articles from across the world about what is happening to the company/stock price. (News API)
+3. Deliver the News on weekly basis to the consumer and 3 articles in S. M. S. format (twilio).
